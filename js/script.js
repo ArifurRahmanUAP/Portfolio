62fe9e54491df9f6ef3c056b54442c61ff8f8d4d
Md. Arifur Rahman <arif659097@gmail.com>
2022-02-12 18:36:12 +0600

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    strings:["","Mobile Application Developer","Android Developer"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
