e9f6b93d209f8748920db10a364d8127155f05b3
Md. Arifur Rahman <arif659097@gmail.com>
2022-09-17 21:10:08 +0600

    CV Updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:[" ","Android Application Developer"],
+    strings:[" ","Android Application Developer", "Software Engineer"],
     typeSpeed:80,
     BackSpeed:40,
     loop:true
